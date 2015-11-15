365daee8c5b8f6dc09cdcf3af849da4bc702193b
Gonçalo Borrêga <goncalo.borrega@gmail.com>
2015-11-15 08:24:28 +0000

    updated script for new ruby-asana library version (#3 & #4)

--- exportTrelloToAsana.rb
+++ exportTrelloToAsana.rb
@@ -13,12 +13,13 @@ TRELLO_DEVELOPER_PUBLIC_KEY = cnf['trello']['developer_public_key']
 TRELLO_MEMBER_TOKEN = cnf['trello']['member_token']
 
 # Asana keys
-ASANA_API_KEY = cnf['asana']['api_key']
+ASANA_TOKEN = cnf['asana']['personal_token']
 ASANA_ASSIGNEE = 'me'
  
 
 def get_option_from_list(list, title, attribute)
   i=0
+  arr = Array.new
 
   while i == 0 do
     puts title
@@ -26,25 +27,25 @@ def get_option_from_list(list, title, attribute)
     list.each do |item|
       i += 1
       puts "  #{i}) #{item.send(attribute)}"
+      arr << item
     end
 
     i = gets.chomp.to_i
     i = 0 if i <= 0 && i > list.size    
   end
-  return i - 1
+  return arr[i - 1]
 end
- 
 
 Trello.configure do |config|
   config.developer_public_key = TRELLO_DEVELOPER_PUBLIC_KEY
   config.member_token = TRELLO_MEMBER_TOKEN
 end
 
-Asana.configure do |client|
-  client.api_key = ASANA_API_KEY
+client = Asana::Client.new do |c|
+  c.authentication :access_token, ASANA_TOKEN
 end
 
-workspaces = Asana::Workspace.all
+workspaces = client.workspaces.find_all
 
 boards = Trello::Board.all
 boards.each do |board|
@@ -54,19 +55,22 @@ boards.each do |board|
   next unless gets.chomp == 'y' 
 
   # Which workspace to put it in
-  workspace = workspaces[get_option_from_list(workspaces, 
+  workspace = get_option_from_list(workspaces, 
     "Select destination workplace", 
-    "name")]
+    "name")
   puts "Using workspace #{workspace.name}"
 
   # Which project to associate
-  project = workspace.projects[get_option_from_list(workspace.projects, 
+  projects = client.projects.find_by_workspace(workspace: workspace.id)
+  project = get_option_from_list(projects, 
     "Select destination project", 
-    "name")]
+    "name")
   puts " -- Using project #{project.name} --"
 
   puts ' -- Getting users --'
-  users = workspace.users
+  users = Array.new
+  users = client.users.find_all(workspace: workspace.id).take(1000) if workspace.name != "Personal Projects"
+
 
   board.lists.each do |list|
   
@@ -75,39 +79,36 @@ boards.each do |board|
     list.cards.reverse.each do |card|
       puts "  - Card #{card.name}, Due on #{card.due}"
 
-      # Create the task
-      t = Asana::Task.new
-      t.name = card.name
-      t.notes = card.desc
-      t.due_on = card.due.to_date if !card.due.nil?
-
       # Assignee - Try to find by name. Otherwise will be empty
-      t.assignee = nil
+      assignee = "me"
       if !card.member_ids.empty? then
         userList = users.select { |u| 
           u.name == card.members[0].full_name
         }
-        t.assignee = userList[0].id unless userList.empty?
+        assignee = userList[0].id unless userList.empty?
       end
+      due_on = card.due.to_date if !card.due.nil?
 
-      task = workspace.create_task(t.attributes)
+      # Create the task
+      task = client.tasks.create(workspace: workspace.id, 
+        name: card.name,
+        notes: card.desc,
+        due_on: due_on,
+        assignee: assignee)
       
       #Project
-      task.add_project(project.id)
+      task.add_project(project: project.id)
 
       #Stories / Trello comments
       comments = card.actions.select {|a| a.type == 'commentCard'}
       comments.each do |c|
-        task.create_story({:text => c.data['text']})
+        task.add_comment(text: c.data['text'])
       end
 
       #Subtasks
       card.checklists.each do |checklist|
         checklist.check_items.each do |checkItem|
-          st = Asana::Task.new
-          st.name = checkItem['name']
-          st.assignee = nil
-          task.create_subtask(st.attributes)
+          task.add_subtask(name: checkItem['name'], completed: checkItem['state'] == "complete")
         end
       end
 
@@ -115,8 +116,9 @@ boards.each do |board|
 
      # Create each list as an aggregator if it has cards in it
     if !list.cards.empty? then
-      task = workspace.create_task({name: "#{list.name}:", assignee: nil})
-      task.add_project(project.id)
+      task = client.tasks.create(workspace: workspace.id, 
+        name: "#{list.name}:")
+      task.add_project(project: project.id)
     end
 
   end
